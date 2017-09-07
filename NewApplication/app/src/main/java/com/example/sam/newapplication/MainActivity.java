cfd282453cbfb5263a552fe6c10bdfab8b9f6e27
koumingyang <1761674434@qq.com>
2017-09-07 10:54:05 +0800

    Runnable features & definitions

--- NewApplication/app/src/main/java/com/example/sam/newapplication/MainActivity.java
+++ NewApplication/app/src/main/java/com/example/sam/newapplication/MainActivity.java
@@ -4,25 +4,92 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-import android.widget.Button;
+import android.widget.TextView;
+import android.os.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView newsList = null;
+    private Handler messageHandler;
+    private Handler oneNewsMessageHandler;
+    private Looper looper;
+    String newsId = "";
+    News singleNews;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newsList = (TextView) findViewById(R.id.news_list);
+        looper = Looper.myLooper();
+        messageHandler = new MessageHandler(looper);
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                String jsonString = "";
+                LatestNewsAccesser accesser = new LatestNewsAccesser();
+                jsonString = accesser.stringBuilder.toString();
+                jsonAnalyserList analyser = new jsonAnalyserList(jsonString);
+                String output = "";
+
+                for (int i = 0; i < analyser.newsList.size(); i++)
+                {
+                    output += analyser.newsList.get(i).news_Title + "\n";
+                    newsId = analyser.newsList.get(i).news_ID;
+                }
+
+                //output += str;
+                //output += jsonString;
+                //output += newsId;
+                //output += oneAnalyser.news.news_Content;
+                //output += tmpId;
+
+                //output += jsonString;
+                Message message = Message.obtain();
+                message.obj = output;
+                messageHandler.sendMessage(message);
+            }
+        }).start();
     }
 
     public void btOnClick(View v) {
-        Intent intent = new Intent(MainActivity.this, ShowDetails.class);
-        intent.putExtra("Headline", "This is a headline");
-        String longString = "";
-        for (int i=0;i<100;i++){
-            longString += "This is a bunch of details.\n";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                oneNewsMessageHandler = new OneNewsMessageHandler(looper);
+                OneNewsAccesser oneAccesser = new OneNewsAccesser(newsId);
+                String jsonOneString = oneAccesser.stringBuilder.toString();
+                jsonAnalyserOne oneAnalyser = new jsonAnalyserOne(jsonOneString);
+                singleNews = oneAnalyser.news;
+                Message message = Message.obtain();
+                message.obj = singleNews;
+                oneNewsMessageHandler.sendMessage(message);
+            }
+        }).start();
+    }
+
+    class MessageHandler extends Handler {
+        public MessageHandler(Looper looper) {
+            super(looper);
         }
-        intent.putExtra("Details", longString);
-        startActivity(intent);
+        @Override
+        public void handleMessage(Message msg) {
+            newsList.append((String) msg.obj);
+        }
+    }
+
+    class OneNewsMessageHandler extends Handler{
+        public OneNewsMessageHandler(Looper looper) {
+            super(looper);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(MainActivity.this, ShowDetails.class);
+            News tmp = (News)msg.obj;
+            intent.putExtra("Headline", tmp.news_Title);
+            String longString = tmp.news_Content;
+            intent.putExtra("Details", longString);
+            startActivity(intent);
+        }
     }
 }
---
+++ NewApplication/app/src/main/java/com/example/sam/newapplication/jsonAnalyserOne.java
@@ -0,0 +1,35 @@
+package com.example.sam.newapplication;
+
+import org.json.*;
+import java.util.*;
+
+/**
+ * Created by kmy on 2017/9/7.
+ */
+
+public class jsonAnalyserOne {
+    News news = new News();
+    jsonAnalyserOne (String jsonString) {
+        try {
+            JSONObject list = new JSONObject(jsonString.toString());
+            news.newsClassTag = list.getString("newsClassTag");
+            news.news_ID = list.getString("news_ID");
+            news.news_Source = list.getString("news_Source");
+            news.news_Title = list.getString("news_Title");
+            news.news_Time = list.getString("news_Time");
+            news.news_URL = list.getString("news_URL");
+            news.news_Author = list.getString("news_Author");
+            news.lang_Type = list.getString("lang_Type");
+            news.news_Pictures = list.getString("news_Pictures");
+            news.news_Video = list.getString("news_Video");
+            news.inborn_KeyWords = list.getString("inborn_KeyWords");
+            news.news_Content = list.getString("news_Content");
+            news.crawl_Source = list.getString("crawl_Source");
+            news.news_Journal = list.getString("news_Journal");
+            news.crawl_Time = list.getString("crawl_Time");
+            news.repeat_ID = list.getString("repeat_ID");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
