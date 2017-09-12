f049ba3ebc1dbf287e514be728118740b68e250b
yaochao <940609536@qq.com>
2017-09-12 11:27:18 +0800

    splash

---
+++ Group37/app/src/main/java/com/java/group37/newsapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.java.group37.newsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 2500; // 两秒后进入系统
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new android.os.Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
+}
