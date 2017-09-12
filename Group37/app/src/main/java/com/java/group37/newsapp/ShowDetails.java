f796261a6793fe4560abf789a12d5f5b94557e2a
zhufangwei <zfw15@mails.tsinghua.edu.cn>
2017-09-12 23:43:10 +0800

    storage issues

--- Group37/app/src/main/java/com/java/group37/newsapp/ShowDetails.java
+++ Group37/app/src/main/java/com/java/group37/newsapp/ShowDetails.java
@@ -1,6 +1,7 @@
 package com.java.group37.newsapp;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
@@ -179,8 +181,24 @@ public class ShowDetails extends AppCompatActivity {
         }
     }
 
+    private void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        final int REQUEST_EXTERNAL_STORAGE = 1;
+        String[] PERMISSIONS_STORAGE = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+    }
 
     private void showShare() {
+        verifyStoragePermissions(this);
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
